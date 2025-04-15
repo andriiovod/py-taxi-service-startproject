541fc6173beb0d362935e20cee9543c046a4e633
Andrii Ovod <ovodenok@mail.com>
2025-04-15 13:33:20 +0300

    Add .gitignore file

---
+++ taxi/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE,
+                                     related_name='cars')
+    drivers = models.ManyToManyField("Driver", related_name='cars')
+
+    def __str__(self):
+        return f"{self.manufacturer} ({self.model})"
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.license_number})"
